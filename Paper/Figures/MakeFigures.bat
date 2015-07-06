d7c42026b090d3455249be77512b4704d307d900
Atrix256 <alan.wolfe@gmail.com>
2015-07-05 21:10:45 -0700

    Technique Extensions

--- Paper/Figures/MakeFigures.bat
+++ Paper/Figures/MakeFigures.bat
@@ -1,1 +1,2 @@
-D:\texlive\2015\bin\win32\pdflatex.exe FigTexture3D.tex
+D:\texlive\2015\bin\win32\pdflatex.exe Decasteljeau.tex
+D:\texlive\2015\bin\win32\pdftopng.exe Decasteljeau.pdf Decasteljeau.png
