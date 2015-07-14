71d8f6950037256061c8b56e85118fab84307416
Atrix256 <alan.wolfe@gmail.com>
2015-07-14 16:02:41 -0700

    Progress - almost all citations in!

--- Paper/MakePaperC.bat
+++ Paper/MakePaperC.bat
@@ -1,1 +1,2 @@
+C:\texlive\2015\bin\win32\bibtex.exe paper
 C:\texlive\2015\bin\win32\pdflatex.exe paper.tex
--- Paper/MakePaperD.bat
+++ Paper/MakePaperD.bat
@@ -1,1 +1,2 @@
+D:\texlive\2015\bin\win32\bibtex.exe paper
 D:\texlive\2015\bin\win32\pdflatex.exe paper.tex
