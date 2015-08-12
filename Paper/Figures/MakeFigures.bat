d808bd987ba3689b1320627eab5d6c7f72ef1499
Atrix256 <alan.wolfe@gmail.com>
2015-08-12 06:55:26 -0700

    Adding paper feedback and random figures made

--- Paper/Figures/MakeFigures.bat
+++ Paper/Figures/MakeFigures.bat
@@ -1,2 +1,2 @@
-D:\texlive\2015\bin\win32\pdflatex.exe Decasteljeau.tex
-D:\texlive\2015\bin\win32\pdftopng.exe Decasteljeau.pdf Decasteljeau.png
+D:\texlive\2015\bin\win32\pdflatex.exe ForwardDifference.tex
+D:\texlive\2015\bin\win32\pdftopng.exe ForwardDifference.pdf ForwardDifference.png
